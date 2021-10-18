6d23b3114647ad1346c08501fe7018936ef48bf8
bjerkt <86378702+bjerkt@users.noreply.github.com>
2021-10-18 18:00:32 -0400

    Forking cleanup

--- AssemblerUPSGrade-SEFork/migrations/110_1_prior.lua
+++
@@ -1,6 +0,0 @@
-for index, force in pairs(game.forces) do
-  local technologies = force.technologies
-  local recipes = force.recipes
-
-  recipes["asif-oil-block"].enabled = technologies["asif"].researched
-end
